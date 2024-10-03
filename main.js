9d0024955dda27ad709e9a11e7be2a7497b323a5
CodeAlchemy24 <eo.69.mzx@gmail.com>
2024-10-03 19:14:25 +0200

    svg circle added

--- main.js
+++ main.js
@@ -32,9 +32,25 @@ fetch('logs.json') // A json fájl neve vagy elérési útja
 
             
  let mainE = document.getElementById("main");
+ /* let svgCircle = document.createElement("circle");
+ svgCircle.setAttribute('id', 'circle1');
+ svgCircle.setAttribute('cx', '100');
+ svgCircle.setAttribute('cy', '100');
+ svgCircle.setAttribute('r', '40');
+ svgCircle.setAttribute('fill', "red");
+ 
+ let svgImg = document.createElement("svg");
+ svgImg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+ svgImg.setAttribute('id', 'minisvg');
+  svgImg.setAttribute('width', '100');
+ svgImg.setAttribute('height', '100'); 
+ 
 
+ svgImg.appendChild(svgCircle); */
   mainE.appendChild(myList1);
-
+ /* mainE.appendChild(svgImg);  */
   mainE.appendChild(myList2);
+
+
 
  
